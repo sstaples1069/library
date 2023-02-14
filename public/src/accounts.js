025682f6f792f75d9aad0ed36c08686e3e2e19fd
sstaples1069 <scottstarkstaples@gmail.com>
2023-02-13 20:33:44 -0800

    revised

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,30 +1,3 @@
-const accounts = [
-  {
-    id: "5f446f2ecfaf0310387c9603",
-    picture: "https://api.adorable.io/avatars/75/esther.tucker@zillacon.me",
-    age: 25,
-    name: {
-      first: "Esther",
-      last: "Tucker",
-    },
-    company: "ZILLACON",
-    email: "esther.tucker@zillacon.me",
-    registered: "Thursday, May 28, 2015 2:51 PM",
-  },
-  {
-    id: "5f446f2ed46724f41c9fc431",
-    picture: "https://api.adorable.io/avatars/75/ferrell.morris@ecolight.com",
-    age: 35,
-    name: {
-      first: "Ferrell",
-      last: "Morris",
-    },
-    company: "ECOLIGHT",
-    email: "ferrell.morris@ecolight.com",
-    registered: "Thursday, February 8, 2018 1:16 PM",
-  },
-];
-
 function findAccountById(accounts, id) {
   let result = accounts.find((account) => {
     return account.id === id;
@@ -76,31 +49,51 @@ function getTotalNumberOfBorrows(account = {}, books = []) {
   return totalCount;
 }
 
+/*goes in after line: const { id } 
+books.forEach((bookObj) => {
+  const { borrows, authorId } = bookObj;
+  borrows.forEach((borrower) => {
+    //have to match borrower id
+    if (borrower.id === id) {
+      //where borrows:returned === false, means that book is possessed
+      if (borrower.returned === false) {
+        const bookAuthor = authors.find((author) => {
+          return author.id === authorId;
+        });
+        bookObj.author = bookAuthor;
+        result.push(bookObj);
+      }
+    }
+  });
+});
+return result;
+*/
 function getBooksPossessedByAccount(account = {}, books = [], authors = []) {
   //getting an account obj and array of all book objs and array of all author objs
   //return an array of all book objs including author info currently checked out by account id
   //will have to find borrows similar to previous function
   const { id } = account;
-  let result = [];
-  books.forEach((bookObj) => {
+  const result = books.filter((bookObj) => {
     const { borrows, authorId } = bookObj;
-    borrows.forEach((borrower) => {
-      //have to match borrower id
-      if (borrower.id === id) {
-        //where borrows:returned === false, means that book is possessed
-        if (borrower.returned === false) {
-          const bookAuthor = authors.find((author) => {
-            return author.id === authorId;
-          });
-          bookObj.author = bookAuthor;
-          result.push(bookObj);
+    const doesAccountPossess = borrows.some((borrower) => {
+      //need to also check if returned
+      if (borrower.returned === false) {
+        return borrower.id === id;
         }
-      }
     });
+    if (doesAccountPossess === true) {
+        const matchingAuthor = authors.find((author) => {
+        return author.id === authorId
+      });
+      bookObj.author = matchingAuthor;
+      return bookObj;
+    }
   });
+  //console.log(result);
   return result;
 }
 
+//git push -u origin main
 module.exports = {
   findAccountById,
   sortAccountsByLastName,
